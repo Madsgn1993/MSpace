5eaee477f368e500657a4158d8cb7c0dd9a7b5b9
Madsgn1993 <mad.sgn1993@gmail.com>
2021-04-30 10:50:29 +0200

    Je l'ai enfin eu ce beugue #riremalefique

--- src/component/elementQuizz/StructureQuiz2.js
+++ src/component/elementQuizz/StructureQuiz2.js
@@ -1,19 +1,24 @@
-import React, { Component } from "react";
-import TabQuestionSaturn  from "../../data/QuestionReponseSaturn";
+import React, { Component, useState } from "react";
+import TabQuestionSaturn from "../../data/QuestionReponseSaturn";
+import Saturne from "../../component/pages/univers1/planeteUniver1/planeteSaturne/Saturne";
 
 class TestQuiz extends Component {
-  
   constructor() {
     super();
+    //this.state === state
+    //C'est dans this.state que l'on déclare les state
     this.state = {
       point: 0,
       reponseChoisis: "",
       bonnereponse: "7",
       idQuestion: 1,
+      maxQuestion: 5,
+      btnDisabled: true,contentQ:1
     };
+    //Approfndire le bind();
     this.onChangeValue = this.onChangeValue.bind(this);
   }
-  
+
   onChangeValue(event) {
     console.log(event.target.value);
   }
@@ -24,39 +29,43 @@ class TestQuiz extends Component {
     });
   };
 
-  incremente = (e) => {
+
+  submitAnswer = (selecteReponse) => {
     this.setState({
-      point: this.state.point + 1,
+      reponseChoisis: selecteReponse,
+      btnDisabled: false,
     });
   };
+
+  nextQuestion = () => {
+    this.setState((prevState) => ({
+      idQuestion: prevState.idQuestion + 1
+    }));
+  };
+
+   handleAnswer = (event) =>{
+   
+        this.setState({contentQ:event.target.value + 1})
+      
+    }
+ 
+  
   // FONCTION SUBMIT ( quand tu appuis sur le bouton )
   handelSubmitForm = (e) => {
-
     e.preventDefault();
     console.log(`ReponseChoisiFinal : ${this.state.reponseChoisis} `);
     if (this.state.reponseChoisis === this.state.bonnereponse) {
-      this.incremente();
       alert("Tu as trouvé la bonne réponse ");
-      
-      
-
     } else {
       alert("Saturne à 7 anneaux ");
     }
   };
 
-  nextQuestion = (e) => {
-
-
-
-
-
-  }
-//----------------
+  //----------------
   render() {
     return (
       <div>
-        <form id="contentQuiz" onSubmit={this.handelSubmitForm} >
+        <form id="contentQuiz" onSubmit={this.handelSubmitForm}>
           <h2>{this.props.Question}</h2>
 
           <div>
@@ -65,6 +74,7 @@ class TestQuiz extends Component {
               name="saturn"
               value={this.props.Reponse1}
               onChange={this.handleReponseDonner}
+              onClick={() => this.submitAnswer(this.props.Reponse1)}
             ></input>
             <label for="reponse1">{this.props.Reponse1}</label>
           </div>
@@ -74,6 +84,7 @@ class TestQuiz extends Component {
               name="saturn"
               value={this.props.Reponse2}
               onChange={this.handleReponseDonner}
+              onClick={() => this.submitAnswer(this.props.Reponse2)}
             ></input>
             <label for="reponse2">{this.props.Reponse2}</label>
           </div>
@@ -83,11 +94,15 @@ class TestQuiz extends Component {
               name="saturn"
               value={this.props.Reponse3}
               onChange={this.handleReponseDonner}
+              onClick={() => this.submitAnswer(this.props.Reponse3)}
             ></input>
             <label for="reponse3">{this.props.Reponse3}</label>
           </div>
-          <button>Valider</button>
+          <button disabled={this.state.btnDisabled}>
+            Validez
+          </button>
         </form>
+        {/* <button onClick={this.handleAnswer}>Next</button> */}
       </div>
     );
   }
--- src/component/pages/univers1/planeteUniver1/planeteSaturne/Saturne.js
+++ src/component/pages/univers1/planeteUniver1/planeteSaturne/Saturne.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import TabQuestionSaturn from "../../../../../data/QuestionReponseSaturn";
 import BossSaturne from "../planeteSaturne/BossSaturne";
 import TestQuiz from "../../../../elementQuizz/StructureQuiz2";
 import testUtils from "react-dom/test-utils";
+import { getAllByPlaceholderText } from "@testing-library/dom";
 //importer Tab Question Saturn
 const Saturne = () => {
   //Afficher Q & R
@@ -10,15 +11,19 @@ const Saturne = () => {
   //Ne renvoye que la premiere question
   //useState (1) => cible l'id du premimer tableau
 
-  let [contentQ, setcontentQ] = useState(1);
+  const [contentQ, setcontentQ] = useState(1);
 
-//DEMANDER COMMENT INCREMENTER LE USESTATE
-  setcontentQ(() => {
-      return {
-        contentQ: contentQ + 1,
-      };
-    });
   
+  // c'est le contentQ qui doit avoi ++ quand on a clicker sur le bouton
+
+ const handleAnswer = (isCorrect) =>{
+   if(isCorrect){
+     const nextQuestion = contentQ + 1
+     if(nextQuestion){
+       setcontentQ(nextQuestion)
+     }
+   }
+ }
 
   return (
     <div>
@@ -32,13 +37,15 @@ const Saturne = () => {
               Reponse1={item.reponses[0]}
               Reponse2={item.reponses[1]}
               Reponse3={item.reponses[2]}
-            ></TestQuiz>
+              
+            >
+              
+            </TestQuiz>
           );
         }
-
-        setcontentQ = contentQ;
-        setcontentQ++;
       })}
+         <button onClick={handleAnswer}>Next</button> 
+    
     </div>
   );
 };
